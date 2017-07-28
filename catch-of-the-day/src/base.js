df82e53eacfd4792063d5d1c44a435b33f976379
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-07-28 16:14:48 +1200

    created and connected firebase db

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyDI9e5pzOY9wJ_8QS2uRBx5hMkymJyUPQ4',
+  authDomain: 'catch-of-the-day-jattob.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-jattob.firebaseio.com'
+})
+
+export default base
