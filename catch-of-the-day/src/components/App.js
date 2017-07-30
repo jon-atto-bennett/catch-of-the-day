eac38cd6f865a471488beadae909ccec7d37f721
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-07-31 11:39:53 +1200

    remove fish from order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
     this.removeFish = this.removeFish.bind(this)
     this.loadSamples = this.loadSamples.bind(this)
     this.addToOrder = this.addToOrder.bind(this)
+    this.removeFromOrder = this.removeFromOrder.bind(this)
     // getinitialState
     this.state = {
       fishes: {},
@@ -86,6 +87,12 @@ class App extends React.Component {
     this.setState({ order })
   }
 
+  removeFromOrder (key) {
+    const order = {...this.state.order}
+    delete order[key] // remove item from order
+    this.setState({ order })
+  }
+
   render () {
     return (
       <div className='catch-of-the-day'>
@@ -104,6 +111,7 @@ class App extends React.Component {
           fishes={this.state.fishes}
           order={this.state.order}
           params={this.props.params}
+          removeFromOrder={this.removeFromOrder}
          />
         <Inventory
           addFish={this.addFish}
